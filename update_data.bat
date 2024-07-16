fbc44579cf81185ab36e7f1cee17632e94fe6bce
julongindonesia <wanghaoyuan@julongindonesia.com>
2024-07-16 15:35:06 +0700

    update data 鑷姩鑴氭湰鎺ㄩ€?

---
+++ update_data.bat
@@ -0,0 +1,9 @@
+python extract_title_v3.0.py
+python extract_data_v3.0.py
+python merge_data_v3.0.py
+
+git add .
+git status
+git commit -m "update data 自动脚本推送"
+git status
+git push -u origin master
