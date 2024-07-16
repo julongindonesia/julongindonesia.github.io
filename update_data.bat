46dd0c443041408f918f718e5010ed12c92827e8
julongindonesia <wanghaoyuan@julongindonesia.com>
2024-07-16 16:23:24 +0700

    update

--- update_data.bat
+++ update_data.bat
@@ -1,11 +1,14 @@
+@REM 从google excel中抽取数据
 python extract_title_v3.0.py
 python extract_data_v3.0.py
 python merge_data_v3.0.py
 
+@REM 将更新的数据提交到github仓库
 git add .
 git status
 git commit -m "update data - Script auto push"
 git status
 git push -u origin main
 
+@REM 等待输入后再关闭控制台
 set /p=
