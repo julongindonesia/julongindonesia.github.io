122b48bf3152269339a39240acaa86c48d9e9c3f
julongindonesia <wanghaoyuan@julongindonesia.com>
2024-09-25 11:41:43 +0700

    update data - Script auto push

--- update_data.bat
+++ update_data.bat
@@ -1,7 +1,7 @@
 @REM 从google excel中抽取数据
-python extract_title_v4.0.py
-python extract_data_v4.0.py
-python merge_data_v3.0.py
+python extract_title_v4.py
+python extract_data_v4.py
+python merge_data_v3.py
 
 @REM 将更新的数据提交到github仓库
 git add .
